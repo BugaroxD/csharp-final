b0807c0f5bd453c57eee19c25f13e4c26eee5058
Jefferson <jeffeluiz97@gmail.com>
2022-05-02 22:09:40 -0300

    feat: criado o login falta testar

---
+++ Views/lib/Fields.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace lib
+{
+  public class Field
+  {
+    public string id;
+    public TextBox textBox;
+    public Label label;
+
+    public Field(
+        string id,
+        int xPoint,
+        int yPoint,
+        string label,
+        int xSize = 240,
+        int ySize = 15,
+        char passwordChar = ' '
+    )
+    {
+      this.id = id;
+
+      this.label = new Label();
+      this.label.Text = label;
+      this.label.Location = new Point(xPoint, yPoint);
+
+      this.textBox = new TextBox();
+      this.textBox.Location = new Point(xPoint, yPoint + 25);
+      this.textBox.Size = new Size(xSize, ySize);
+
+      if (!Char.IsWhiteSpace(passwordChar))
+      {
+        this.textBox.PasswordChar = passwordChar;
+      }
+    }
+  }
+}
